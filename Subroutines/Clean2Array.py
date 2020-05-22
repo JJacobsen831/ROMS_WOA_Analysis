2880707f9c189bbc7105055b8c2b09dac00550bf
Jasen Jacobsen <jasen.robert.jacobsen@gmail.com>
2020-05-22 13:05:21 -0700

    added subroutine folder

---
+++ Subroutines/Clean2Array.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 21 11:13:34 2020
+
+@author: Jasen
+"""
+import numpy as np
+
+var1 = np.array([1, 2, 3, np.nan, 5])
+var2 = np.array([1, np.nan, 3, 4, 5])
+
+def Clean2Array(var1, var2):
+    """ removes nans from two arrays maintaining index size """
+    if var1.size == var2.size :
+        #find nan in array 1
+        inan1 = np.argwhere(np.isnan(var1))
+                
+        #delete location of nan in array 2
+        new_var2 = np.delete(var2, inan1)
+        new_var1 = np.delete(var1, inan1)
+        
+        #find nan in new array 2
+        inan2 = np.argwhere(np.isnan(new_var2))
+        clean_var1 = np.delete(new_var1, inan2)
+        clean_var2 = np.delete(new_var2, inan2)
+        
+        Arrays = (clean_var1, clean_var2)
+        
+    else :
+        Arrays = 'arrays not the same size'
+        
+    return Arrays
+        
+    
